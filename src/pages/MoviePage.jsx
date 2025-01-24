2a1c1129897e62b8117f8fd75b3e17dd4be6625a
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-24 17:33:05 +0100

    feat: pausa <3

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
 function MoviePage () {
+    const [movies, setMovies] = useState([]);
+     useEffect(()=>{
+         axios.get("http://localhost:3000").then((resp)=> {
+             setMovies(resp.data.data)
+         })
+     })
 return(
 <>
 <h1>sono MoviePage</h1>
 
 </>
-
 )
-
 };
 
 export default MoviePage;

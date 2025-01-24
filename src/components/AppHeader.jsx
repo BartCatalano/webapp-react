c6b474f4edbfde0ae15af7f802889df3c8e24cab
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-24 17:14:45 +0100

    feat: navLink creati in maniera dinamico gestendo la classe active. Home Page (WIP)

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,8 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+
 function AppHeader() {
-    return(
+
+    const navLinks = [
+        {
+            path: "/",
+            title: "Home"
+        }, {
+            path: "/movies",
+            title: "Movies"
+        }
+
+
+    ]
+    return (
         <>
-            
-            <h1>sono header</h1>
+            <header className="HeaderLayout" >
+                <div className="container">
+                <div className="row">
+                    <h2>Logo Pagina</h2>
+                    <h1>Titolo Pagina</h1>
+                    <div className="col">
+                        {navLinks.map((curLink, index) => (
+                            <div key={index}>
+                                <NavLink
+                                    to={curLink.path} 
+                                    className="linkButton" >
+                                    {curLink.title}
+                                </NavLink>
+                            </div>
+                        ))}
+                    </div>
+                    </div>
+                    </div>
+
+            </header>
+
 
         </>
     )

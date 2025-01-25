0ab8ca7ba634d87257760c19444ef4cea9c43409
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-25 11:55:33 +0100

    feat: esercizio base fatto. bonus (WIP)

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,38 +1,58 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import AppCard from "../components/AppCart";
+import AppCard from "../components/AppCard";
 
 
 
-function MoviePage () {
+function MoviePage() {
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState("");
 
-
-
-
-     useEffect(()=>{
-         axios.get("http://localhost:3000").then((resp)=> {
-             setMovies(resp.data.data)
+    const getMovies = () => {
+         const params = {};
+         if (search.length > 0) {
+            params.search = search;
+         }
+        axios.get("http://localhost:3000", {params}).then((resp) => {
+            setMovies(resp.data.data)
              console.log(resp.data.data);
-             
-             
-             
-         });
-     },[]);
-     
-return(
-<>
-<section className="container">
-{movies.map((curMovie, id) =>
-<div key={curMovie.id}>
-    
-    <AppCard movies={curMovie} />
-</div>
+            
 
-)}
-</section>
-</>
-)
-};
+        });
+    };
+     
+    useEffect(() =>{
+        getMovies()
+       
+        
+    },[]);
+
+
+
+
+
+return (
+    <>
+        <section className="container">
+             <div>
+                <input 
+                value={search}
+                onChange={(event)=> setSearch(event.target.value)}
+                type="search" />
+
+                <button onClick={getMovies}>Cerca</button>
+            </div> 
+
+
+            {movies.map((curMovie, id) =>
+                <div key={curMovie.id}>
+
+                    <AppCard movies={curMovie} />
+                </div>
+
+            )}
+        </section>
+    </>
+) }
 
 export default MoviePage;

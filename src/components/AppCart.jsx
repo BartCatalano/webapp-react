755c492ce36a68313c95e1a89324f2a486c48cab
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-25 10:37:58 +0100

    chiamata funzionante, appCard inserita in appmovies

---
+++ src/components/AppCart.jsx
@@ -0,0 +1,12 @@
+function AppCard () {
+ 
+
+    return (
+        <>
+
+<h1>sono appcard</h1>
+        </>
+    )
+}
+
+export default AppCard;
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import AppCard from "../components/AppCart";
 
 
 
 function MoviePage () {
     const [movies, setMovies] = useState([]);
+
+
+
+
      useEffect(()=>{
          axios.get("http://localhost:3000").then((resp)=> {
              setMovies(resp.data.data)
-         })
-     })
+             console.log(resp.data.data);
+             
+             
+             
+         });
+     },[]);
+     
 return(
 <>
-<h1>sono MoviePage</h1>
+{movies.map((curMovie, id) =>
+<div key={curMovie.id}>
+    {curMovie.title}
+    <AppCard />
+</div>
+
+)}
 
 </>
 )

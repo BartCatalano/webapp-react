4823a1c83d7a8f4bf44bab0fb04e51ea08eaee90
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-28 16:16:50 +0100

    feat: spostata la zona dettagli e impostato lo spazio per il form.

--- src/pages/DettailsPage.jsx
+++ src/pages/DettailsPage.jsx
@@ -8,60 +8,66 @@ import { useParams } from "react-router-dom";
 function DettailsPage() {
 
 
-const { id } = useParams(); 
+    const { id } = useParams();
 
-const [ movieDetails, setMovieDetails] = useState(null);
-const [reviews, setReviews] = useState([]);
+    const [movieDetails, setMovieDetails] = useState(null);
+    const [reviews, setReviews] = useState([]);
 
-useEffect(() => {
-    axios.get(`http://localhost:3000/${id}`).then((resp) => {
-        setMovieDetails(resp.data.data); // Salva i dettagli del film nello stato
-        setReviews(resp.data.data.review);
-    });
-}, [id]); // La useEffect si attiva quando l'ID cambia
+    useEffect(() => {
+        axios.get(`http://localhost:3000/${id}`).then((resp) => {
+            setMovieDetails(resp.data.data); // Salva i dettagli del film nello stato
+            setReviews(resp.data.data.review);
+        });
+    }, [id]); // La useEffect si attiva quando l'ID cambia
 
 
     return (
-        <> 
-         <div>
-            {movieDetails ? (
-                <div>
-                    <h2>{movieDetails.title}</h2>
-                    <img src={`http://localhost:3000/${movieDetails.image}`} alt={movieDetails.title} />
-                    <p>{movieDetails.abstract}</p>
-                    <p><strong>Genere:</strong> {movieDetails.genre}</p>
-                    <p><strong>Regista:</strong> {movieDetails.director}</p>
-                    {/* Puoi aggiungere altre informazioni come le recensioni qui */}
+        <>
+            <section className="rowReview">
+                <div className="colReview">
+                    {movieDetails ? (
+                        <div>
+                            <div className="titleCard"> <h2>{movieDetails.title}</h2> </div>
+                            <div className="imgContainer"> <img className="imgDettails" src={`http://localhost:3000/${movieDetails.image}`} alt={movieDetails.title} /> </div>
+                            <div className="sectionRow"><strong>Genere:</strong>  {movieDetails.genre}</div>
+                            <div className="sectionRow"><strong>Regista: </strong>  {movieDetails.director} </div>
+                            <div className="sectionRow">
+                                <p><strong>Descrizione:</strong> {movieDetails.abstract}</p></div>
+
+
+
+                        </div>
+                    ) : (
+                        <p>Loading...</p>
+                    )}
                 </div>
-            ) : (
-                <p>Loading...</p>
-            )}
-        </div>
-    );
-
-       
-        
 
 
-        {/* sezione review */}
 
-          <section className="reviewSection">
-                    <div className="rowReview mt20">
+
+
+                <section className="colreview">
+                    <section className="form">
+                        <p>qui inserisco form</p>
+                    </section>
+                </section> 
+                 </section>
+
+
+                {/* sezione review */}
+                <section className="reviewBotRow">
+                    
                         {reviews.map((curReview, id) =>
-                            <div className="colReview" key={id}>
+                            <div className="reviewCard" key={id}>
                                 <div className="titleRev">  </div>
-
                                 <div className="textRev"> Recensione fatta da:  {curReview.name}
                                     <div className="pdt15" >  Commento:  {curReview.text}</div>
                                     <div className="pdt15"> voto: {curReview.vote}</div></div>
-
                             </div>
-
-
                         )}
-                    </div>
-                </section> 
-        
+                    
+                </section>
+          
         </>
     )
 }
